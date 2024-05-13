c42fd486e23810312dccdef4b0f6480d4f314abb
Kadir Balkaya <122707602+kadirbalkaya10@users.noreply.github.com>
2024-05-13 15:15:24 -0400

    Exporting typeDefs,resolvers from index.js file

---
+++ server/schemas/index.js
@@ -0,0 +1,7 @@
+// Importing type definitions from typeDefs.js file
+const typeDefs = require("./typeDefs");
+// Importing resolver functions from resolvers.js file
+const resolvers = require("./resolvers");
+
+// Exporting type definitions and resolver functions
+module.exports = { typeDefs, resolvers };
