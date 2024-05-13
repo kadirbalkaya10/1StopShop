d86d2e12a55d29f3d0c021dd443ee7a42a9a49fc
Pema Sherpa <pematamang111@gmail.com>
2024-05-13 12:35:22 -0400

    add db once open

--- server/server.js
+++ server/server.js
@@ -23,5 +23,19 @@ app.use(express.json());
 
 app.use('./graphql', expressMiddleware(server));
 
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/dist')));
 
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    });
+}
 
+db.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`API server running on port ${PORT}!`);
+        console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+    })
+});
+
+startApolloServer();
