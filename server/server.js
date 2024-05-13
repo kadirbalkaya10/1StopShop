fb998414a1f9110a5f39410b470c1cf56de426a2
Pema Sherpa <pematamang111@gmail.com>
2024-05-13 12:22:23 -0400

    add new instance of apolloserver

--- server/server.js
+++ server/server.js
@@ -6,3 +6,10 @@ const path = require('path');
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
+const PORT = process.env.PORT || 3001;
+const app = express();
+const server = new ApolloServer({
+    typeDefs, 
+    resolvers,
+});
+
