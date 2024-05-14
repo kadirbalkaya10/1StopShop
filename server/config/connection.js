8e5d6b3f7a060621b8e3b7701672304768b42454
Kadir Balkaya <122707602+kadirbalkaya10@users.noreply.github.com>
2024-05-14 12:28:50 -0400

    Mango db connection from render

--- server/config/connection.js
+++ server/config/connection.js
@@ -4,8 +4,7 @@ const mongoose = require("mongoose");
 // Connect to the MongoDB database
 mongoose.connect(
   // Use the MongoDB URI specified in the environment variable or default to a local MongoDB instance
-  "mongodb+srv://kadirbalkaya08:tsc7YtVnTYconWeY@cluster0.lho2c3o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" ||
-    "mongodb://127.0.0.1:27017/onestopshopDB"
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/onestopshopDB"
 );
 
 //Export db connection
