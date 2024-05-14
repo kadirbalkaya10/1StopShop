21e0ca2def9da3a870d4e063566b12d4a37c0437
mayachakra <mayajchakravarty@gmail.com>
2024-05-14 11:39:58 -0400

    added remaining seed data except order

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,12 +1,18 @@
 const db = require('../config/connection');
-const { User } = require('../models');
+const { User, Category, Product } = require('../models');
 const userSeeds = require('./userSeeds.json');
+const categorySeeds = require('./categorySeeds.json');
+const productSeeds = require('./productSeeds.json');
 const cleanDB = require('./cleanDB');
 
 db.once('open', async () => {
-  await cleanDB('User', 'user');
+  await cleanDB('User', 'user'); //maybe capitalize second one
+  await cleanDB('Category', 'category');
+  await cleanDB('Product', 'product');
 
   await User.create(userSeeds);
+  await Category.create(categorySeeds);
+  await Product.create(productSeeds);
 
   console.log('all done!');
   process.exit(0);
