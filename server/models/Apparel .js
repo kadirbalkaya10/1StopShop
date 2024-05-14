e2314717a9be94b6e44a585992e229bdec602fbc
Pema Sherpa <pematamang111@gmail.com>
2024-05-13 23:44:15 -0400

    add apparelScheme instance

--- server/models/Apparel .js
+++ server/models/Apparel .js
@@ -1,3 +1,11 @@
 // Importing Mongoose library
 const mongoose = require("mongoose");
 
+
+// Destructuring Schema from mongoose
+const { Schema } = mongoose;
+
+// Defining the schema for the apparel collection
+const apparelScheme = new Schema({
+
+})
