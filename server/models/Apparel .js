9cabd089427740d0436b35ef548c293bd7e8f221
Pema Sherpa <pematamang111@gmail.com>
2024-05-14 00:21:16 -0400

    export apparel from apparel.js

--- server/models/Apparel .js
+++ server/models/Apparel .js
@@ -1,11 +1,50 @@
 // Importing Mongoose library
-const mongoose = require("mongoose");
-
+const mongoose = require('mongoose');
 
 // Destructuring Schema from mongoose
 const { Schema } = mongoose;
 
 // Defining the schema for the apparel collection
 const apparelScheme = new Schema({
+	category: {
+		type: Int,
+		required: true,
+	},
+	product: [
+		{
+			productId: {
+				type: Int,
+				required: true,
+			},
+			product_name: {
+				type: String,
+				required: true,
+			},
+			price: {
+				type: Int,
+				required: true,
+			},
+			stock: {
+				type: Int,
+				required: true,
+			},
+		},
+	],
+	Tag: [
+		{
+			tagId: {
+				type: Int,
+				required: true,
+			},
+			tag_name: {
+				type: String,
+			},
+		},
+	],
+});
 
-})
+// Creating a model named "Apparel" based on the userSchema
+const Apparel = mongoose.model("Apparel", apparelScheme);
+
+// Exporting the Apparel model
+module.exports = Apparel;
