41c82dbfe123731a814a63d951eb85f296f217de
Pema Sherpa <pematamang111@gmail.com>
2024-05-14 00:55:47 -0400

    add typedefs for Apparel

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -6,8 +6,41 @@ type User {
     email:String
 }
 
+type Apparel {
+   category: Number
+
+   product: [{
+    productId: Number
+    product_name: String
+    stock: Number
+    image: String
+   }] 
+
+   tag: [{
+    tagId: Number
+    tag_name: String
+   }]
+}
+
 type Query {
-    user:User
+    user: User
+    apparel: [Apparel]
+}
+
+type Mutation {
+    login(email: String!, password: String): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addApparel(category: [Number!], 
+        product: [{ 
+            productId: Number!,
+            product_name: String!,
+            stock: Number!,
+            image: String!}], 
+            
+        tag: [{
+            tagId: Number!,
+            tag_name: String!}] ): User
+    removeApparel(productId: ID!): User
 }
 `;
 
