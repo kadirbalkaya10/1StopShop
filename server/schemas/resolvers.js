4d9887be2353e0e529b7c803e28bf3c5538dab6f
Pema Sherpa <pematamang111@gmail.com>
2024-05-14 13:36:48 -0400

    change Auth.js to auth.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,16 +1,35 @@
-const { User, Product, Category } = require("../models");
+//importing models
+const { User, Product, Category, Order } = require("../models");
+//importing auth
+const { signToken, AuthenticationError } = require('../utils/auth.js');
+//stripe for payment
+const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
+
 
 const resolvers = {
   Query: {
-    user: async () => {
-      return await User.find({});
-    },
     category: async () => {
       return await Category.find({});
     },
-    product: async () => {
-      return await Product.find({});
-    }
+    products: async (parent, {category, name}) => {
+      const params = {};
+
+      if(category){
+        params.category = category;
+      }
+      if(name) {
+        params.name = {
+          $regex: name,
+        }
+      }
+      return await Product.find(params).populate('category');
+    },
+    product: async (parent, {_id}) => {
+        return await Product.find(_id).populate('category');
+    },
+    user: async () => {
+      return await User.find({});
+    },
   },
 };
 
