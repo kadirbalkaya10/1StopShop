888af430905ac08f7f95dffe1fb4788443a1a251
ECS <ethansantana93@gmail.com>
2024-05-14 21:29:54 -0400

    removed nav from homepage and added unsued code to navbar.jsx for later use or reference

---
+++ client/src/Nav.jsx
@@ -0,0 +1,32 @@
+// Navbar code. If new code added then you can delete this
+
+// {/* Navbar */}
+// <nav className="bg-white shadow-lg w-full">
+//   <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+//     <div className="flex justify-between items-center h-16">
+//       <a href="#" className="text-2xl font-bold text-gray-900">
+//         1StopShop
+//       </a>
+//       <div className="hidden sm:flex sm:space-x-8">
+//         <a
+//           href="#"
+//           className="text-gray-500 hover:text-gray-700 transition duration-300"
+//         >
+//           Tops
+//         </a>
+//         <a
+//           href="#"
+//           className="text-gray-500 hover:text-gray-700 transition duration-300"
+//         >
+//           Bottoms
+//         </a>
+//         <a
+//           href="#"
+//           className="text-gray-500 hover:text-gray-700 transition duration-300"
+//         >
+//           Shoes
+//         </a>
+//       </div>
+//     </div>
+//   </div>
+// </nav>;
