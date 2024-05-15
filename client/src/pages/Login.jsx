74d5e72adce76963b14614b245ccf2ba9dee0075
ECS <ethansantana93@gmail.com>
2024-05-15 11:33:28 -0400

    commit after npm i

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,82 +1,86 @@
 // reference Login.jsx in module 24 solutions
 import { Link } from "react-router-dom";
 //add additional dependencies
-import { useState } from 'react';
-import { useMutation } from '@apollo/client';
-import { LOGIN_USER } from '../utils/mutations';
-import Auth from '../utils/auth';
+// import { useState } from 'react';
+// import { useMutation } from '@apollo/client';
+// import { LOGIN_USER } from '../utils/mutations';
+// import Auth from '../utils/auth';
 
-const Login = () => { //do we add props?
+const Login = () => {
+  //do we add props?
   //add code to
   //useState for form and useMutation for login
-  const [formState, setFormState] = useState({ email: '', password: ''});
-  const [login, {error, data}] = useMutation(LOGIN_USER);
+  // const [formState, setFormState] = useState({ email: "", password: "" });
+  // const [login, { error, data }] = useMutation(LOGIN_USER);
 
-  //update state on form input changes
-  handleChange
-  const handleChange = (event) => {
-    const { name, value } = event.target;
+  // //update state on form input changes
+  // handleChange;
+  // const handleChange = (event) => {
+  //   const { name, value } = event.target;
 
-    setFormState({
-      ...formState,
-      [name]: value,
-    });
-  };
+  //   setFormState({
+  //     ...formState,
+  //     [name]: value,
+  //   });
+  // };
 
-  handleFormSubmit
-  const handleFormSubmit = async (event) => {
-    event.preventDefault();
-    console.log(formState);
-    try{
-      const { data } = await login({ //login from the auth.js
-        variables: {...formState},
-      });
-      Auth.login(data.login.token);
-    } catch (error){
-      console.error(error);
-    }
-     //clear form values with setFormState
-    setFormState({
-      email: '',
-      password: '',
-    });
-  }
+  // handleFormSubmit;
+  // const handleFormSubmit = async (event) => {
+  //   event.preventDefault();
+  //   console.log(formState);
+  //   try {
+  //     const { data } = await login({
+  //       //login from the auth.js
+  //       variables: { ...formState },
+  //     });
+  //     Auth.login(data.login.token);
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  //   //clear form values with setFormState
+  //   setFormState({
+  //     email: "",
+  //     password: "",
+  //   });
+  // };
 
   return (
-    <section className='skeleton w-full h-screen flex justify-center items-center flex-col'>
-      <form action='submit' className='w-11/12'>
-        <h1 className='text-4xl text-white mb-12'>Login</h1>
-        <label className='input input-bordered bg-primary text-white flex items-center gap-2 m-3'>
+    <section className="skeleton w-full h-screen flex justify-center items-center flex-col">
+      <form action="submit" className="w-11/12">
+        <h1 className="text-4xl text-white mb-12">Login</h1>
+        <label className="input input-bordered bg-primary text-white flex items-center gap-2 m-3">
           <svg
-            xmlns='http://www.w3.org/2000/svg'
-            viewBox='0 0 16 16'
-            fill='currentColor'
-            className='w-4 h-4 opacity-70'>
-            <path d='M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z' />
-            <path d='M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z' />
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 16 16"
+            fill="currentColor"
+            className="w-4 h-4 opacity-70"
+          >
+            <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
+            <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
           </svg>
-          <input type='text' className='grow ' placeholder='Email' />
+          <input type="text" className="grow " placeholder="Email" />
         </label>
 
-        <label className='input input-bordered flex bg-primary text-white items-center gap-2 m-3'>
+        <label className="input input-bordered flex bg-primary text-white items-center gap-2 m-3">
           <svg
-            xmlns='http://www.w3.org/2000/svg'
-            viewBox='0 0 16 16'
-            fill='currentColor'
-            className='w-4 h-4 opacity-70'>
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 16 16"
+            fill="currentColor"
+            className="w-4 h-4 opacity-70"
+          >
             <path
-              fillRule='evenodd'
-              d='M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z'
-              clipRule='evenodd'
+              fillRule="evenodd"
+              d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+              clipRule="evenodd"
             />
           </svg>
-          <input type='password' className='grow' value='password' />
+          <input type="password" className="grow" value="password" />
         </label>
-        <button className='btn w-11/12 h-12  bg-primary text-white  btn-xs sm:btn-sm md:btn-md lg:btn-lg'>
+        <button className="btn w-11/12 h-12  bg-primary text-white  btn-xs sm:btn-sm md:btn-md lg:btn-lg">
           Login
         </button>
       </form>
-      <Link key={1} to={"/signup"} className='mt-5 hover:text-orange-200'>
+      <Link key={1} to={"/signup"} className="mt-5 hover:text-orange-200">
         Signup instead
       </Link>
     </section>
