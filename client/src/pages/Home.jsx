979b915bb44cc7afaf6b6c73670b65767a203776
Pema Sherpa <pematamang111@gmail.com>
2024-05-16 20:31:25 -0400

    remove console log

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -49,7 +49,7 @@ console.log(data);
 
           {data && 
            data.products.map((product, index) => {
-           console.log(products);
+           
            return (
           <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8'  key={index}>
 
