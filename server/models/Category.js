f3c91f5a3a3704a5b61cd5855a2675d0ad3c0caf
mayachakra <mayajchakravarty@gmail.com>
2024-05-17 10:15:03 -0400

    added notes to category

--- server/models/Category.js
+++ server/models/Category.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 // Destructuring Schema from mongoose
 const { Schema } = mongoose;
 
+//makes schema for all of the categories such as tops, bottoms, and shoes
 const categorySchema = new Schema({
     name: {
         type: String,
